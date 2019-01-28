067f36ab65e36a5ca577f30c86a8b31f806f95be
Lin Meyer <linjmeyer@gmail.com>
2019-01-27 20:32:44 -0600

    Changed to ternary

--- toggle-device.ps1
+++ toggle-device.ps1
@@ -9,14 +9,5 @@ $headsetId = "{0.0.0.00000000}.{23fa13ad-9a76-4650-984a-cfd303ad1d1d}"
 $speakersId = "{0.0.0.00000000}.{62570bac-8404-4f40-941d-b6d9278dc923}"
 # Get current device (-Playback gets default)
 $currentDevice = Get-AudioDevice -Playback
-
-if($currentDevice.ID -eq $headsetId) {
-    Set-AudioDevice -ID $speakersId
-} else {
-    Set-AudioDevice -ID $headsetId 
-}
-
-
-
-
-
+# Set new device to whatever isn't the default currently
+Set-AudioDevice -ID $( If ($currentDevice.ID -eq $headsetId) {$speakersId} else {$headsetId} )
